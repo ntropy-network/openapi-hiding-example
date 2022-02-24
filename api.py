9ebb47a8bae8aa354753318e4acec778051c98d3
Bagrat Aznauryan <bagrat@ntropy.network>
2022-02-24 10:12:55 +0400

    Implement x-hidden

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 import connexion
+import yaml
 
 
 app = connexion.FlaskApp('hiding-example')
@@ -16,6 +17,29 @@ def new_shiny_endpoint():
     return "I new and shiny", 201
 
 
+@app.route("/openapi.json", endpoint="_openapi_json")
+def _openapi_json():
+    openapi_yaml_path = "openapi.yaml"
+
+    with open(openapi_yaml_path) as openapi_yaml:
+        openapi = yaml.safe_load(openapi_yaml)
+
+    hidden_endpoints = []
+    for path, methods in openapi["paths"].items():
+        for method, endpoint in methods.items():
+            print(f"{path} {method}")
+            if endpoint.get("x-hidden"):
+                hidden_endpoints.append((path, method))
+
+    for path, method in hidden_endpoints:
+        del openapi["paths"][path][method]
+
+    return openapi
+
+
 if __name__ == '__main__':
-    app.add_api('openapi.yaml')
+    app.add_api(
+        'openapi.yaml',
+        options={"serve_spec": False},
+    )
     app.run(port=8080)
